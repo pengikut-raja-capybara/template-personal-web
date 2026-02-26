87909946b412c8c1e93d463bed156ea515ada501
Raja Capybara <rajacapybara275@gmail.com>
2026-02-26 10:59:26 +0700

    refactor: streamline GitHub Actions workflow and update Vite configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Untuk GitHub Pages user site (username.github.io)
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-})
+  base: process.env.GITHUB_ACTIONS ? "/template-personal-web/" : "/",
+});
